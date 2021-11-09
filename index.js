46650d931d9ffe3d3c68d8abf59a79cd4e341297
developer-Azad <akalamazad500@gmail.com>
2021-11-09 23:07:08 +0600

    users

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aymnc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,7 +16,42 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try{
         await client.connect();
-        console.log('database connected')
+        const database = client.db('doctors_portal');
+        const appointmentCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
+
+      app.get('/appointments', async(req, res) => {
+        const email = req.query.email;
+        const date = new Date(req.query.date).toLocaleDateString();
+        const query = {email: email, date: date}
+        const cursor = appointmentCollection.find(query);
+        const appointments = await cursor.toArray();
+        res.json(appointments);
+      })
+
+        app.post('/appointments', async(req, res) => {
+          const appointment = req.body;
+          const result = await appointmentCollection.insertOne(appointment);
+          console.log(result);
+          res.json(result)
+        })
+        
+        app.post('/users', async(req, res) => {
+          const user = req.body;
+          const result = await usersCollection.insertOne(user);
+          console.log(result);
+          res.json(result)
+        })
+
+        app.put('/users', async(req, res) => {
+          const user = req.body;
+          const filter = {email: user.email};
+          const options = {upsert: true};
+          const updateDoc = {$set: user};
+          const result = await usersCollection.updateOne(filter, updateDoc, options);
+          res.json(result)
+        })
+
     }
     finally{
         // await client.close();
